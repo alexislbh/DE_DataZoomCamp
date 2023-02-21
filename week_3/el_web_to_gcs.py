8863759dee260cdf0ec97d4cfb5a352b65daa5c1
alexislbh <alexis.lbh@gmail.com>
2023-02-21 20:58:43 +0100

    start week_4

--- week_3/el_web_to_gcs.py
+++ week_3/el_web_to_gcs.py
@@ -4,9 +4,9 @@ from prefect import task, flow
 from prefect_gcp.cloud_storage import GcsBucket
 
 @task(retries=3)
-def write_local(url: str, file_name: str) -> Path:
+def write_local(url: str, color: str, file_name: str) -> Path:
     '''Write file to local disk'''
-    path = Path(f'./ny_taxi/fhv/{file_name}.csv.gz')
+    path = Path(f'./week_3/ny_taxi/{color}/{file_name}.csv.gz')
     os.system(f'wget {url} -O {path}')
     return path
 
@@ -24,22 +24,26 @@ def remove_local(path: Path) -> None:
     return
 
 @flow()
-def el_web_to_gcs(year: int, month: int) -> None:
+def el_web_to_gcs(color: str, year: int, month: int) -> None:
     '''ETL from web to GCS'''
-    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/fhv_tripdata_{year}-{month:02}.csv.gz'
-    file_name = f'fhv_tripdata_{year}_{month:02}'
+    #url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/fhv_tripdata_{year}-{month:02}.csv.gz'
+    #file_name = f'fhv_tripdata_{year}_{month:02}'
+    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{color}_tripdata_{year}-{month:02}.csv.gz'
+    file_name = f'{color}_tripdata_{year}_{month:02}'
 
-    path = write_local(url, file_name)
+    path = write_local(url, color, file_name)
     path = write_gcs(path)
     remove_local(path)
 
 @flow
-def el_multi_web_to_gcs(years: list[int]):
-    for year in years:
-        for month in range(12):
-            el_web_to_gcs(year, month + 1)
+def el_multi_web_to_gcs(colors: list[str], years: list[int]):
+    for color in colors:
+        for year in years:
+            for month in range(12):
+                el_web_to_gcs(color, year, month + 1)
 
 if __name__ == '__main__':
-    years = [2019]
-    el_multi_web_to_gcs(years)
+    colors = ['green', 'yellow']
+    years = [2019, 2020]
+    el_multi_web_to_gcs(colors, years)
 
